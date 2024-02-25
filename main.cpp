18b3924998a30e64df6d7f7e3c856060fd345df3
1mask_1 <damianwal95@gmail.com>
2024-02-25 13:32:35 +0100

    added some functionalyty

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 using namespace std;
 
-#define WHITE = 0x00FFFFFF
+#define WHITE {255, 255, 255}
 
 COLORREF get_pixel_color(int x, int y)
 {
@@ -21,6 +21,22 @@ void get_cursor_pos(int& x, int& y)
     y = cursor_pos.y;
 }
 
+std::array<int, 3> color_to_array(COLORREF color)
+{
+    int rgb1 = GetRValue(color);
+    int rgb2 = GetRValue(color);
+    int rgb3 = GetRValue(color);
+    std::array<int, 3> rgb_return = {rgb1, rgb2, rgb3};
+    return rgb_return;
+}
+
+bool copeare_colors(const std::array<int, 3>& color1, const std::array<int, 3>& color2, int tolerance)
+{
+    return (abs(color1[0] - color2[0]) <= tolerance &&
+            abs(color1[1] - color2[1]) <= tolerance &&
+            abs(color1[2] - color2[2]) <= tolerance);
+}
+
 void click()
 {
     mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0 ,0 ,0);
@@ -35,12 +51,24 @@ int main()
     {
         itr++;
         get_cursor_pos(x, y);
-        std::cout << x << " " << y << std::endl;
-        if (itr > 3000)
+        std::cout << x << " " << y << std::ends;
+        COLORREF color = get_pixel_color(x, y);
+        std::cout << " " << color << std::ends;
+        std::array<int, 3> color_rgb = color_to_array(color);
+        for (int i = 0; i < 3; i++)
+        {
+            std::cout << " " << color_rgb[i] << std::ends;
+        }
+
+        bool res = copeare_colors(color_rgb, WHITE, 20);
+        std::cout << " " << res << std::ends;
+
+
+        std::cout << " " << std::endl;
+        if (itr > 300)
         {
             break;
         }
     }
-    click();
     return 0;
 }
