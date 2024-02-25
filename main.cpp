d2942077628fbe253b07d967bc3a8a8a0932948c
1mask_1 <damianwal95@gmail.com>
2024-02-25 13:07:46 +0100

    e

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-
+#include <array>
+#include <windows.h>
 using namespace std;
+
+#define WHITE = 0x00FFFFFF
+
+COLORREF get_pixel_color(int x, int y)
+{
+    HDC hdc = GetDC(NULL);
+    COLORREF color = GetPixel(hdc, x, y);
+    ReleaseDC(NULL, hdc);
+    return color;
+}
+
+void get_cursor_pos(int& x, int& y)
+{
+    POINT cursor_pos;
+    GetCursorPos(&cursor_pos);
+    x = cursor_pos.x;
+    y = cursor_pos.y;
+}
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    int x, y;
+    int itr = 0;
+    while (true)
+    {
+        itr++;
+        get_cursor_pos(x, y);
+        std::cout << x << " " << y << std::endl;
+        if (itr > 30000)
+        {
+            break;
+        }
+    }
     return 0;
 }
